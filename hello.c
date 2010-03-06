935c52c3fc4207aecd20274c1cb4910f04813ccb
Tentacle UK <tentacleuk@googlemail.com>
2010-03-06 20:32:41 +0000

    New feature - say hello N times

--- hello.c
+++ hello.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-	printf("Hello world - you complete bastard\n");
+	if (argc != 2) {
+		fprintf(stderr, "usage: hello times\n");
+		return -1;
+	}
+	int times = atoi(argv[1]);
+	int i = 0;
+	for (; i < times; i++)
+		printf("Hello world - you complete bastard\n");
 	return 0;
 }
