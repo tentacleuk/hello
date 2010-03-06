64125bffced48311166cf5a769a4796dcbd3d909
Tentacle UK <tentacleuk@googlemail.com>
2010-03-06 21:49:46 +0000

    Slightly better usage implementation

--- hello.c
+++ hello.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void usage()
+{
+	fprintf(stderr, "usage: hello times\n");
+	exit(-1);
+}
+
 int main(int argc, char **argv)
 {
-	if (argc != 2) {
-		fprintf(stderr, "usage: hello times\n");
-		return -1;
-	}
+	/* check correct number of parameters */
+	if (argc != 2) usage();
+ 
 	int times = atoi(argv[1]);
+	if (times == 0) usage();
 	int i = 0;
 	for (; i < times; i++)
 		printf("Hello world - you nice fluffy place \n");
